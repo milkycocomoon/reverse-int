6731b32234fc5e042ecd4293b2c55efab750f754
Moon <118666310+milkycocomoon@users.noreply.github.com>
2023-02-12 18:21:21 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    result = parseFloat(n.toString().split('').reverse().join(''));
+    if (n[0] % 2 === 0){
+      return result * -1;
+    }
+    return result
 }
